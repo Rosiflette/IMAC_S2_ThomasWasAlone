e58104ec83e3cf885b9f875ffe9c06aeb7c60b84
Rose <janinejacques.tod@gmail.com>
2022-04-26 14:12:06 +0200

    Initial Commit

---
+++ src/test.cpp
@@ -0,0 +1,19 @@
+// class QuadTree{
+//     childs
+// }
+//
+// class Rectangle{
+//     int x, y;
+//     int length;
+//     int height;
+//     color;
+// }
+//
+// class Joueur{
+//     Rectangle joueur;
+//     collision();
+//     movement();
+//     sauter();
+// }
+//
+// Joueurs = [joueur1, joueur2..]
